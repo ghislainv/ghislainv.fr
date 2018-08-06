1c7c68b2414e51513898efca3c4ec719af7da68d
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2018-08-06 17:42:58 +0200

    Initial commit

---
+++ R/buildsite.R
@@ -0,0 +1,13 @@
+#!/usr/bin/Rscript
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ghislainv.github.io
+# license         :GPLv3
+# ==============================================================================
+
+# Render site. 
+rmarkdown::render_site("site")
+
+# End
